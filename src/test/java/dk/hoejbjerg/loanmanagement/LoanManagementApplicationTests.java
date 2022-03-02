e88bb9a6783a716c66a62ee0901ab956b726c9d0
b33rsledge <45030619+b33rsledge@users.noreply.github.com>
2022-03-02 19:48:50 +0100

    Now with build file

---
+++ src/test/java/dk/hoejbjerg/loanmanagement/LoanManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.hoejbjerg.loanmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
